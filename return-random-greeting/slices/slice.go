11a8e76c4390f774d3d6c3a1f8653427a579cec6
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-08 09:21:47 -0300

    slices(parte2): slices basico

--- return-random-greeting/slices/slice.go
+++ return-random-greeting/slices/slice.go
@@ -1,43 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
+var letters = []string{"A", "B", "C", "D", "E"}
 
-	// Declare and initialize a slice of integers
-	numbers := []int{1, 2, 3, 4, 5}
+func Slice() {
 
-	// Print the original slice
-	fmt.Println("Original slice:", numbers)
+	var letter2 = []string{"A", "B", "C", "D"}
+	fmt.Println(letter2)
 
-	// Append a new element to the slice
-	numbers = append(numbers, 6)
-
-	// Print the updated slice
-	fmt.Println("Updated slice:", numbers)
-
-	// Get the length of the slice
-	length := len(numbers)
-
-	// Print the length of the slice
-	fmt.Println("Length of the slice:", length)
-
-	// Get the capacity of the slice
-	capacity := cap(numbers)
-
-	// Print the capacity of the slice
-	fmt.Println("Capacity of the slice:", capacity)
-
-	// Get the value at index 2
-	value := numbers[2]
-
-	// Print the value at index 2
-	fmt.Println("Value at index 2:", value)
-
-	// Remove the element at index 2
-	numbers = append(numbers[:1], numbers[3])
-	fmt.Println(numbers)
+	leeter3 := []string{"A", "B", "C", "D"}
+	fmt.Println(leeter3)
 
 }
+
+// TODO [FORA DE FUNÇÃO]
+
+// * Eu consigo declarar fora de uma função com a estrutura
+// var "nomeVariavel" = []string{"A", "B", "C", "D"}
+
+// ! Eu não consigo declarar fora de uma função com a estrutura
+// "nomeVariavel" := []string{"A", "B", "C", "D"}
+// ? Porque? -> Em Go, a sintaxe := faz a declaração e inicialização da variável ao mesmo tempo, inferindo o tipo automaticamente. Contudo, isso é válido apenas dentro de funções, porque o escopo global exige uma declaração explícita com var
+// ? Em resumo, a sintaxe := é restrita ao escopo local (dentro de funções)
+
+// TODO [DENTRO DE FUNÇÃO]
+
+// * Eu consigo declarar dentro de uma função com a estrutura
+// var "nomeVariavel" = []string{"A", "B", "C", "D"}
+
+// * Eu consigo declarar dentro de uma função com a estrutura
+// "nomeVariavel" := []string{"A", "B", "C", "D"}
