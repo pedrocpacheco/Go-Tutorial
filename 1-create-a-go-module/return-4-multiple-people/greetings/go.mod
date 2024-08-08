aa791a3f14c645430fb84091233460b3f6c35c04
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-08 10:44:33 -0300

    init copia

---
+++ 1-create-a-go-module/return-4-multiple-people/greetings/go.mod
@@ -0,0 +1,4 @@
+// go mod init example.com/greetings
+module example.com/greetings
+
+go 1.22.5
---
+++ 1-create-a-go-module/return-4-multiple-people/greetings/greetins.go
@@ -0,0 +1,41 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+func randomFormat() string {
+	formats := []string{
+		"Hi %v. Welcome",
+		"Great to see you, %v!",
+		"Hello %v. How u doing?",
+	}
+	return formats[rand.Intn(len(formats))]
+}
+
+// Aprendi a usar MAP
