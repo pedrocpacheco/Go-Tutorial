357276313b43f118636b18a8c6f8d83cec013772
pedrocpacheco <cpedropacheco@hotmail.com>
2024-08-08 08:31:03 -0300

    anotacoes

--- call-a-code-from-another-module/greetings/greetins.go
+++ call-a-code-from-another-module/greetings/greetins.go
@@ -6,11 +6,3 @@ func Hello(name string) string {
 	message := fmt.Sprintf("Hello %s", name)
 	return message
 }
-
-// ! Nome das funções
-// Nome com letra maiucula -> Função Publica
-// Nome com letra minuscula -> Função Privada
-// * No momento de chamar a função também precisa ser Maisculo/minusculo
-
-// ! Operador :=
-// Ele declara e inicializa uma variavel na mesma linha
